005b02d6631920edde2977389bdae40bce5d8f35
cadumeneses <carloseduardoms646@gmail.com>
2023-01-04 10:00:09 -0300

    💥 [LG-2] FEAT: adicionando prametros para LoginComEmail

--- app/lib/app/features/login/domain/usecases/login_com_email_usecase.dart
+++ app/lib/app/features/login/domain/usecases/login_com_email_usecase.dart
@@ -1,5 +1,20 @@
+import 'package:equatable/equatable.dart';
+
 import '../entities/entities.dart';
 
 abstract class LoginComEmail {
   Future<void> autenticacao(LoginCredencialEntity credencial);
+}
+
+class LoginComEmailCredenciais extends Equatable {
+  final String email;
+  final String password;
+
+  @override
+  List get props => [email, password];
+
+  const LoginComEmailCredenciais({
+    required this.email,
+    required this.password,
+  });
 }
