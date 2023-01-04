f293690eeca08adf39dddad0c5fcae2582d32626
cadumeneses <carloseduardoms646@gmail.com>
2023-01-04 09:43:36 -0300

    💥 [LG-5] FEAT: adicionando HttpClientExternal

--- app/lib/app/features/login/external/datasources/http/http_cilent_external.dart
+++ app/lib/app/features/login/external/datasources/http/http_cilent_external.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
 
 import '../../../infra/datasources/http/http.dart';
@@ -8,8 +10,49 @@ class HttpClientExternal implements HttpClient {
   HttpClientExternal(this.client);
 
   @override
-  Future<void> request({String? url, String? method, Map? headers, Map? body}) {
-    throw UnimplementedError();
+  Future<Map<dynamic, dynamic>> request({
+    String? url,
+    String? method,
+    Map? headers,
+    Map? body,
+  }) async {
+    final cabecalho = headers?.cast<String, String>() ?? {}
+      ..addAll(
+        {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        },
+      );
+
+    final jsonBody = jsonEncode(body);
+
+    var response =  Response('', 500);
+
+    Future<Response>? futureResponse;
+
+    try {
+      if (method == 'post') {
+        futureResponse = client.post(Uri.parse(url!), headers: cabecalho, body: jsonBody);
+      }
+      if (futureResponse != null) {
+        response = await futureResponse.timeout(const Duration(seconds: 20));
+      }
+    } catch (e) {
+      throw HttpError.serverError;
+    }
+    return serverResponses(response);
   }
 
+  dynamic serverResponses(Response response) {
+    switch (response.statusCode) {
+      case 200: return response.body.isEmpty ? null : jsonDecode(response.body);
+      case 204: return null;
+      case 400: throw HttpError.badRequest;
+      case 401: throw HttpError.unauthorized;
+      case 403: throw HttpError.forbiden;
+      case 404: throw HttpError.notFound;
+      default: throw HttpError.serverError;
+    }
+  }
+  
 }
