6cb729b2abe3d42ff2e258506a0695580b6a86a7
cadumeneses <carloseduardoms646@gmail.com>
2023-01-04 08:35:25 -0300

    💥 [LG-2] FEAT: adicionando descrição do DomainError : UnexpectedError

--- app/lib/app/features/login/domain/errors/domain_error.dart
+++ app/lib/app/features/login/domain/errors/domain_error.dart
@@ -2,3 +2,14 @@ enum DomainError {
   unexpected,
   invalidCredencials
 }
+
+extension DomainErrorExtension on DomainError {
+  String get descricaoError {
+    switch (this) {
+      case DomainError.unexpected:
+        return 'Algo de errado aconteceu';
+      default:
+        return '';
+    }
+  }
+}
