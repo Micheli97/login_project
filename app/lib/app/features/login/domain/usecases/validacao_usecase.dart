ff90254685979debe40e3895776f3ad49c3fb150
cadumeneses <carloseduardoms646@gmail.com>
2023-01-05 09:45:59 -0300

    [LG-2] FEAT: adicionando ValidacaoCampoUsecase

--- app/lib/app/features/login/domain/usecases/validacao_usecase.dart
+++ app/lib/app/features/login/domain/usecases/validacao_usecase.dart
@@ -1,3 +1,8 @@
 abstract class Validacao {
   String? validar({required String campo, required String valor});
 }
+
+abstract class ValidacaoCampoUsecase {
+  String get campo;
+  String? validarCampo(String value);
+}
