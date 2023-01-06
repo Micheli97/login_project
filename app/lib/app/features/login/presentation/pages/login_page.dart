aa4a57785095c8648e4db40841efa3a170e6b0e0
cadumeneses <carloseduardoms646@gmail.com>
2023-01-06 11:13:42 -0300

    [LG-7] REFACTOR: movendo Widget CabecalhoLogin para sua pasta

--- app/lib/app/features/login/presentation/pages/login_page.dart
+++ app/lib/app/features/login/presentation/pages/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:app/app/features/login/presentation/pages/login_presenter.dart';
 import 'package:flutter/material.dart';
 
+import 'components/components.dart';
+
 class LoginPage extends StatefulWidget {
   final LoginPresenter presenter;
   const LoginPage({required this.presenter, super.key});
@@ -65,34 +67,7 @@ class _LoginPageState extends State<LoginPage> {
       return SingleChildScrollView(
         child: Column(
           children: [
-            Stack(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(40),
-                          bottomRight: Radius.circular(40)),
-                      color: Colors.white),
-                  width: MediaQuery.of(context).size.width,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * .6,
-                  padding: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.15),
-                  child: const Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Text(
-                      'Entre na sua conta',
-                      style: TextStyle(
-                          color: Colors.blueGrey,
-                          fontSize: 45,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            const CabecalhoLogin(),
             Form(
               child: Column(
                 children: [
